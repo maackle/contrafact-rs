0b0a3fb5a553b20912296b754d57874aec840994
Michael Dougherty <maackle.d@gmail.com>
2021-05-05 19:41:04 -0700

    Add useless StatefulFact

---
+++ src/stateful.rs
@@ -0,0 +1,25 @@
+//! This doesn't do anything useful, because the State cannot inform the Facts.
+
+use crate::{fact::Bounds, Fact, FactVec};
+
+pub struct StatefulFact<'a, S, T> {
+    state: S,
+    update: Box<dyn Fn(&mut S)>,
+    facts: FactVec<'a, T>,
+}
+
+impl<'a, S, T> Fact<T> for StatefulFact<'a, S, T>
+where
+    T: Bounds,
+{
+    fn check(&mut self, obj: &T) -> crate::fact::CheckResult {
+        let result = self.facts.check(obj);
+        (self.update)(&mut self.state);
+        result
+    }
+
+    fn mutate(&mut self, obj: &mut T, u: &mut arbitrary::Unstructured<'static>) {
+        self.facts.mutate(obj, u);
+        (self.update)(&mut self.state);
+    }
+}
