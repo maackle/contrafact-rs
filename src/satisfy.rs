67f038e5712d083dba44980f9e41d132d134160b
Michael Dougherty <maackle.d@gmail.com>
2023-06-29 19:56:42 -0700

    0.2.0-dev.4

--- src/satisfy.rs
+++ src/satisfy.rs
@@ -1,8 +1,7 @@
 /// Convenience macro for creating a collection of [`Fact`](crate::Fact)s
 /// of different types.
-/// Each Fact will be boxed and added to a Vec as a trait object, with their
-/// types erased.
-/// The resulting value also implements `Fact`.
+/// The Facts will be composed into a nested series of [`AndFact`] which causes
+/// all facts to be applied in sequence. The collection of Facts is also a Fact.
 ///
 /// ```
 /// use contrafact::*;
@@ -22,16 +21,5 @@ macro_rules! facts {
             $crate::AndFact::new($fact_0, $fact_1),
             $( $fact_n , )*
         ]
-    }};
-}
-
-/// Box the result of [`facts!`]
-#[macro_export]
-macro_rules! boxfacts {
-
-    ( $($fact_n:expr),+ ) => {{
-        Box::new(facts![
-            $( $fact_n ),+
-        ])
     }};
 }
